527b1de0dfdfe4b9562203b5956068fe137f8786
JuliaGerasymenko <gerasymenko.j.ke@gmail.com>
2020-06-05 20:54:09 +0300

    fixup

--- cmd/lb/balancer.go
+++ cmd/lb/balancer.go
@@ -1,38 +1,61 @@
 package main
 
 import (
+	"container/heap"
 	"context"
 	"flag"
 	"fmt"
+	"hash/fnv"
 	"io"
 	"log"
 	"net/http"
 	"time"
-	"hash/fnv"
-	"errors"
-
 
 	"github.com/Vlad1slavIP74/GO3lab/httptools"
 	"github.com/Vlad1slavIP74/GO3lab/signal"
 )
 
-
 var (
-	port = flag.Int("port", 8090, "load balancer port")
+	port       = flag.Int("port", 8090, "load balancer port")
 	timeoutSec = flag.Int("timeout-sec", 3, "request timeout time in seconds")
-	https = flag.Bool("https", false, "whether backends support HTTPs")
+	https      = flag.Bool("https", false, "whether backends support HTTPs")
 
 	traceEnabled = flag.Bool("trace", false, "whether to include tracing information into responses")
 )
 
 var (
 	timeout = time.Duration(*timeoutSec) * time.Second
-	serversPool = map[string]bool {
-		"server1:8080": true,
-		"server2:8080": true,
-		"server3:8080": true,
-	}
 )
+
+type ServerInfo struct {
+	server  string
+	healthy bool
+	number  int
+}
+
+type healthyServersPool []int
+
+func (h healthyServersPool) Len() int {
+	return len(h)
+}
+
+func (h healthyServersPool) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+func (h healthyServersPool) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *healthyServersPool) Pop() interface{} {
+	old := *h
+	ret := old[len(old)-1]
+	*h = old[:len(old)-1]
+	return ret
+}
+
+func (h *healthyServersPool) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
 
 func scheme() string {
 	if *https {
@@ -89,55 +112,51 @@ func forward(dst string, rw http.ResponseWriter, r *http.Request) error {
 	}
 }
 
+func healthyState(server string, number int, pq *healthyServersPool) {
+	if health(server) {
+		heap.Push(pq, number)
+		*pq = append(*pq, number)
+		fmt.Println(pq.Len(), number)
+	}
+}
+
 func hash(s string) int {
 	h := fnv.New32a()
 	h.Write([]byte(s))
 	return int(h.Sum32())
 }
 
-func addressHash(servers map[string]bool, clientAddress string) (string, error) {
-	if (clientAddress == "") {
-		return "", errors.New("BAD ADDRESS")
-	}
-
-	var availableServers []string
-
-	for server, isAvailable := range servers {
-		if isAvailable {
-			availableServers = append(availableServers, server)
-		}
-	}
-
-
-	if len(availableServers) == 0 {
-		return "", errors.New("ALL SERVERS ARE NOT HEALTH")
-	}
-
+func addressHash(clientAddress string, pq *healthyServersPool, heap []ServerInfo) string {
 	checksum := hash(clientAddress)
-	serverIndex := checksum % len(availableServers)
-	availableServer := availableServers[serverIndex]
-	return availableServer, nil
+	serverIndex := checksum % pq.Len()
+	availableServer := heap[(*pq)[serverIndex]].server
+	return availableServer
 }
 
 func main() {
 	flag.Parse()
 
-	for server := range serversPool {
-		server := server
+	pq := &healthyServersPool{}
+	serverHeap := []ServerInfo{
+		{"server1:8080", true, 0},
+		{"server2:8080", true, 1},
+		{"server3:8080", true, 2},
+	}
+
+	for _, value := range serverHeap {
+		server := value.server
+		number := value.number
 		go func() {
 			for range time.Tick(10 * time.Second) {
-				log.Println(server, health(server))
-				serversPool[server] = health(server)
+				log.Println(server, health(server), number)
+				healthyState(server, number, pq)
 			}
 		}()
 	}
 
 	frontend := httptools.CreateServer(*port, http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		targetServer, err := addressHash(serversPool, r.Header.Get("X-Forwarded-For"))
-		if err != nil {
-			log.Println(err)
-			rw.WriteHeader(http.StatusServiceUnavailable)
-		} else {
+		if pq.Len() != 0 {
+			targetServer := addressHash(r.Header.Get("X-Forwarded-For"), pq, serverHeap)
 			forward(targetServer, rw, r)
 		}
 	}))
